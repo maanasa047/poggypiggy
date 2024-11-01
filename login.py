385f53993b653fe91b85d694f690093fde0b3f56
ABC <abc@gmail.com>
2024-11-01 11:04:58 +0530

    login.py is commttted

---
+++ login.py
@@ -0,0 +1,6 @@
+print("pig")
+
+SR201@DESKTOP-LFG7KAT MINGW64 /c/cloud (master)
+$ git push origin master
+remote: Permission to maanasa047/poggypiggy.git denied to Balaji1720.
+fatal: unable to access 'https://github.com/maanasa047/poggypiggy.git/': The requested URL returned error: 403
